7d497d42628b27a104362597907d76e3f36971ad
yosif11 <49305870+yosif11@users.noreply.github.com>
2020-08-05 15:46:19 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.listen(() => console.log("start btrolie"));
+
+app.use('/ping', (req, res) => {
+  res.send(new Date());
+});
+var shell = require('shelljs'); 
+shell.exec('java -jar JMusicBot-0.2.9.jar')
